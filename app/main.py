fae33ab3afb0a9123bf7e59d573c30f0d1260fb2
filmza1992 <ddza1992@hotmail.com>
2023-08-19 17:21:41 +0700

    Dockerfile and requirements

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import aiohttp
 import pickle
-from code import predictcar
+from app.code import predictcar
 import os
 app = FastAPI()
 
@@ -16,13 +16,12 @@ app.add_middleware(
 class Item(BaseModel):
     img: str 
 
-PATH_HOG = 'http://localhost:8080/api/genhog'
+PATH_HOG = 'http://172.17.0.2:80/api/genhog'
 
-HOG_API_URL_DEFAULT = 'http://localhost:8080/api/genhog'
-HOG_API_URL_ALTERNATE = 'http://172.17.0.1:80/api/genhog'
+
 HEADERS = {"Content-Type": "application/json"}
 
-model = pickle.load(open(r'..\\model\\ImageFeatureModel.pk', 'rb'))
+model = pickle.load(open(os.getcwd()+r'/model/ImageFeatureModel.pk', 'rb'))
 
 # pwd == work
 
